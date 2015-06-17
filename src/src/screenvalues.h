e63c9016c489c5d1005fd576b63a370ac540b575
Iktwo SH <iktwo.sh@gmail.com>
2015-06-16 20:26:55 -0700

    Add margins for status bar and navigation bar

--- src/src/screenvalues.h
+++ src/src/screenvalues.h
@@ -3,35 +3,67 @@
 
 #include <QObject>
 
+#include "system.h"
+
 class ScreenValues : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(int statusBarHeight READ statusBarHeight NOTIFY statusBarHeightChanged)
+    Q_PROPERTY(int navigationBarHeight READ navigationBarHeight NOTIFY navigationBarHeightChanged)
+    Q_PROPERTY(int navigationBarHeightLandscape READ navigationBarHeightLandscape NOTIFY navigationBarHeightLandscapeChanged)
     Q_PROPERTY(int dpi READ dpi WRITE setDpi NOTIFY dpiChanged)
     Q_PROPERTY(float dp READ dp WRITE setDp NOTIFY dpChanged)
     Q_PROPERTY(bool isTablet READ isTablet NOTIFY isTabletChanged)
+    Q_PROPERTY(bool navBarVisible READ navBarVisible NOTIFY navBarVisibleChanged)
+
 public:
     explicit ScreenValues(QObject *parent = 0);
 
-    int dpi() const;
-    void setDpi(int dpi);
+    Q_INVOKABLE void updateScreenValues();
 
     float dp() const;
     void setDp(float dp);
 
+    int dpi() const;
+    void setDpi(int dpi);
+
+    int statusBarHeight() const;
+
+    int navigationBarHeight() const;
+
+    int navigationBarHeightLandscape() const;
+
     bool isTablet() const;
     void setIsTablet(bool isTablet);
 
+    bool navBarVisible() const;
+
 signals:
-    void dpiChanged();
     void dpChanged();
+    void dpiChanged();
+    void statusBarHeightChanged();
+    void navigationBarHeightChanged();
+    void navigationBarHeightLandscapeChanged();
     void isTabletChanged();
+    void navBarVisibleChanged();
 
 private:
-    int m_dpi;
+    System m_system;
     float m_dp;
+    int m_dpi;
+    int m_statusBarHeight;
+    int m_navigationBarHeight;
+    int m_navigationBarHeightLandscape;
     bool m_isTablet;
+    bool m_navBarVisible;
 
     int retrieveDpi();
+    int getResourceSize(const QString &resource);
+
+    void setStatusBarHeight(int statusBarHeight);
+    void setNavigationBarHeight(int navigationBarHeight);
+    void setNavigationBarHeightLandscape(int navigationBarHeightLandscape);
+    void setNavBarVisible(bool visible);
 };
 
 #endif // SCREENVALUES_H
